77183008c3d10c39ec62c24a70ab32608ab41d9f
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2018-11-18 18:58:36 +0100

    Cleaning up example code, and setting up code style & PHPStorm

--- front/src/router.js
+++ front/src/router.js
@@ -14,11 +14,16 @@ export default new Router({
       component: Home
     },
     {
+      path: '/rankings',
+      name: 'rankings',
+      // route level code-splitting
+      // this generates a separate chunk (rankings.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "rankings" */ './views/Rankings.vue')
+    },
+    {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
--- front/src/views/Rankings.vue
+++ front/src/views/Rankings.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div>Rankings</div>
 </template>
 
 <script>
 export default {
-  name: 'about'
+  name: 'rankings'
 }
 </script>
 
