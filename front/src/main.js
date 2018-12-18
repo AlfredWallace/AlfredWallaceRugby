178af7895d4dffdec96b3c4fd7abf44c0c381911
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2018-12-18 22:30:35 +0100

    First draft of attaching touch events

--- front/src/main.js
+++ front/src/main.js
@@ -8,5 +8,26 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  data: {
+    xPosInit: null,
+    xPos: null,
+    swipeThreshold: 30
+  },
+  mounted: function () {
+    let vm = this
+
+    vm.$el.addEventListener('touchstart', function (event) {
+      vm.xPosInit = vm.xPos = event.touches[0].clientX
+    })
+    vm.$el.addEventListener('touchmove', function () {
+      vm.xPos = event.touches[0].clientX
+    })
+    vm.$el.addEventListener('touchend', function () {
+      console.log('xPosInit', vm.xPosInit)
+      console.log('xPos', vm.xPos)
+      console.log('abs(xPosInit - xPos)', Math.abs(vm.xPosInit - vm.xPos))
+      console.log('abs(xPosInit - xPos) > 30 ?', Math.abs(vm.xPosInit - vm.xPos) > vm.swipeThreshold)
+    })
+  }
 }).$mount('#app')
