19610881f8e3fa122833e2acbe9c32f82cf0fb51
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2019-08-15 02:58:07 +0200

    Using v-data-table with a functional component for the Ranking cells

--- src/components/RankCell.vue
+++ src/components/RankCell.vue
@@ -1,8 +1,7 @@
-<template >
-  <div class="d-flex flex-nowrap">
-    <span><slot name="first"></slot></span>
-    &nbsp;
-    <span><slot name="second"></slot></span>
+<template functional v-bind="data.attrs">
+  <div class="d-flex align-center flex-nowrap" :class="data.class">
+    <span class="subtitle-1"><slot name="first"></slot></span>&nbsp;
+    <span class="font-weight-light subtitle-2"><slot name="second"></slot></span>
   </div>
 </template>
 
--- src/views/Rankings.vue
+++ src/views/Rankings.vue
@@ -5,33 +5,78 @@
         Rankings
       </v-card-title>
 
-<!--      <v-simple-table>-->
-<!--        <thead>-->
-<!--        <tr>-->
-<!--          <th></th>-->
-<!--          <th>Rank</th>-->
-<!--          <th>Team</th>-->
-<!--          <th class="text-right">Points</th>-->
-<!--        </tr>-->
-<!--        </thead>-->
-<!--        <tbody class="font-weight-bold">-->
-<!--        <RankRow v-for="team in normalizedTeams" :key="team.id" :team="team"></RankRow>-->
-<!--        </tbody>-->
-<!--      </v-simple-table>-->
+      <v-data-table
+        :headers="headers"
+        :items="normalizedTeams"
+        :items-per-page="normalizedTeams.length"
+        :mobile-breakpoint="0"
+        disable-pagination
+        hide-default-footer
+      >
+        <template v-slot:item.actions="{ item }">
+          <v-icon>mdi-arrow-up-bold-box</v-icon>
+        </template>
+
+        <template v-slot:item.rank="{ item }">
+          <RankCell>
+            <template v-slot:first>{{ item.rank }}</template>
+            <template v-slot:second>({{ item.previousRank }})</template>
+          </RankCell>
+        </template>
+
+        <template v-slot:item.team="{ item }">
+          <RankCell :class="{ 'flex-column': $vuetify.breakpoint.xsOnly }">
+            <template v-slot:first>
+              <v-img src="../assets/nz.svg" max-width="40px"></v-img>
+            </template>
+            <template v-slot:second>{{ $vuetify.breakpoint.xsOnly ? item.abbr : item.name }}
+            </template>
+          </RankCell>
+        </template>
+
+        <template v-slot:item.points="{ item }">
+          <RankCell :class="{ 'flex-column': $vuetify.breakpoint.xsOnly }">
+            <template v-slot:first>{{ item.roundedPoints }}</template>
+            <template v-slot:second>({{ item.roundedPreviousPoints }})</template>
+          </RankCell>
+        </template>
+
+      </v-data-table>
     </v-card>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
-import RankRow from '../components/RankRow.vue';
+import { mapGetters } from 'vuex';
+import RankCell from '../components/RankCell.vue';
 
 export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: '',
+          value: 'actions',
+        },
+        {
+          text: 'Rank',
+          value: 'rank',
+        },
+        {
+          text: 'Team',
+          value: 'team',
+        },
+        {
+          text: 'Points',
+          value: 'points',
+        },
+      ],
+    };
+  },
   components: {
-    RankRow,
+    RankCell,
   },
   computed: {
-    ...mapState(['teams']),
     ...mapGetters(['normalizedTeams']),
   },
   methods: {},
