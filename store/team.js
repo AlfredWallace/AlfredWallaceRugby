6390170a68ceb3786a81d6ce0d8937441ab3b3f7
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2020-03-11 00:09:18 +0100

    Refactoring the store to match Nuxt practices

---
+++ store/team.js
@@ -0,0 +1,69 @@
+import Team from '../classes/Team'
+import roundingHelper from '../services/roundingHelper.js'
+
+function normalizeTeams(apiTeams) {
+  return apiTeams.map(
+    (team) =>
+      new Team(
+        team.team.id,
+        team.team.name,
+        team.team.abbreviation,
+        team.pos,
+        team.pts
+      )
+  )
+}
+
+export const state = () => ({
+  teams: []
+})
+
+export const mutations = {
+  INIT_TEAMS: (state, teams) => {
+    state.teams = teams
+  },
+  RESET_STEPS: (state) => {
+    state.teams.forEach((team) => {
+      team.ranks = [team.ranks.shift()]
+      team.points = [team.points.shift()]
+    })
+  },
+  ADD_STEP_FOR_TEAM: (state, { id, rank, points }) => {
+    const team = state.teams.find((teamToFind) => teamToFind.id === id)
+    team.ranks.push(rank)
+    team.points.push({
+      raw: points,
+      rounded: roundingHelper.roundPoints(points)
+    })
+  },
+  INIT_NEW_STEP: (state, idsBlackList) => {
+    for (let i = 0; i < state.teams.length; i++) {
+      if (idsBlackList.includes(state.teams[i].id)) {
+        continue
+      }
+
+      const team = state.teams[i]
+      const points = team.points[team.points.length - 1]
+
+      team.ranks.push(team.ranks[team.ranks.length - 1])
+      team.points.push({
+        raw: points.raw,
+        rounded: points.rounded
+      })
+    }
+  }
+}
+
+export const actions = {
+  initTeams: ({ commit }, teams) => {
+    commit('INIT_TEAMS', normalizeTeams(teams))
+  },
+  resetSteps: ({ commit }) => {
+    commit('RESET_STEPS')
+  },
+  makeNewStep: ({ commit }, payload) => {
+    commit('INIT_NEW_STEP', [payload.home.id, payload.away.id])
+    commit('ADD_STEP_FOR_TEAM', payload.home)
+    commit('ADD_STEP_FOR_TEAM', payload.away)
+  }
+}
