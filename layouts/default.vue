ee04bc523bf2c882dab9a924e702f86e0042fd90
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2020-03-14 15:07:55 +0100

    Using the nuxt store methods instead of old vuex code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,58 +1,50 @@
 <template>
-  <div id="app">
-    <v-app>
-      <v-app-bar app>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-        <v-toolbar-title>Alfred Wallace Rugby</v-toolbar-title>
-      </v-app-bar>
+      <v-toolbar-title>Alfred Wallace Rugby</v-toolbar-title>
+    </v-app-bar>
 
-      <v-navigation-drawer v-model="drawer" touchless app>
-        <v-subheader>Navigation</v-subheader>
+    <v-navigation-drawer v-model="drawer" touchless app>
+      <v-subheader>Navigation</v-subheader>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-list nav>
-          <v-list-item link :to="{ name: 'home' }" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <span>Home</span>
-            </v-list-item-content>
-          </v-list-item>
+      <v-list nav>
+        <v-list-item link :to="{ name: 'index' }" exact>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <span>Home</span>
+          </v-list-item-content>
+        </v-list-item>
 
-          <v-list-item link :to="{ name: 'about' }" exact>
-            <v-list-item-icon>
-              <v-icon>mdi-information</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <span>About</span>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+        <v-list-item link :to="{ name: 'about' }" exact>
+          <v-list-item-icon>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <span>About</span>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-content>
-        <router-view></router-view>
-      </v-content>
-    </v-app>
-  </div>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
-
 export default {
-  name: 'App',
+  fetch({ store }) {
+    store.dispatch('initStore')
+  },
   data: () => ({
     drawer: null
-  }),
-  created() {
-    this.initStore()
-  },
-  methods: {
-    ...mapActions(['initStore'])
-  }
+  })
 }
 </script>
