a26aaf52491a2f044193e1753c37b3bcc3a16d93
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2020-03-22 19:54:54 +0100

    Setting up a state initialisation + commenting unused actions/mutations in store

--- store/index.js
+++ store/index.js
@@ -1,74 +1,99 @@
 export const state = () => ({
   currentStep: 0,
-  rankingFreshness: null
+  rankingFreshness: null,
+  loading: false,
+  errorMessage: null
 })
 
 export const mutations = {
-  INIT_STATE: (state, timestamp) => {
+  INIT_STATE_LOADING: (state) => {
+    state.loading = true
+  },
+  INIT_STATE_SUCCESS: (state, timestamp) => {
+    state.loading = false
     state.rankingFreshness = timestamp
+    state.errorMessage = null
   },
-  STEP_UP: (state) => {
-    state.currentStep += 1
-  },
-  STEP_DOWN: (state) => {
-    state.currentStep -= 1
+  INIT_STATE_FAILURE: (state, { message }) => {
+    state.loading = false
+    state.errorMessage = message
   }
+  // STEP_UP: (state) => {
+  //   state.currentStep += 1
+  // },
+  // STEP_DOWN: (state) => {
+  //   state.currentStep -= 1
+  // }
 }
 
 export const actions = {
   async nuxtServerInit({ commit, dispatch }) {
-    const worldRugbyData = await this.$axios.$get(
-      'http://cmsapi.pulselive.com/rugby/rankings/mru.json'
-    )
-    dispatch('team/initTeams', worldRugbyData.entries)
-    commit('INIT_STATE', worldRugbyData.effective.millis)
-  },
-  stepUp({ commit }) {
-    commit('STEP_UP')
-  },
-  stepDown({ commit }) {
-    commit('STEP_DOWN')
-  },
-  calculate({ dispatch }) {
-    // if (!this.$refs.matchForm.validate()) {
-    //   return;
-    // }
-    //
-    // const match = this.matches[this.index];
-    // const homeAdvantage = match.neutralGround ? 0 : 3;
-    // const homePoints = match.home.team.points[this.currentStep].raw;
-    // const awayPoints = match.away.team.points[this.currentStep].raw;
-    //
-    // let pointsDifference = homePoints + homeAdvantage - awayPoints;
-    // if (pointsDifference < -10) {
-    //   pointsDifference = -10;
-    // } else if (pointsDifference > 10) {
-    //   pointsDifference = 10;
-    // }
-    //
-    // const scoreDifference = match.home.score - match.away.score;
-    // let p;
-    // if (scoreDifference > 0) {
-    //   p = (-10 - scoreDifference) / 10;
-    // } else if (scoreDifference < 0) {
-    //   p = (10 - scoreDifference) / 10;
-    // } else {
-    //   p = (-scoreDifference) / 10;
-    // }
-    //
-    // if (Math.abs(scoreDifference) > 15) {
-    //   p *= 1.5;
-    // }
-    //
-    // if (match.worldCup) {
-    //   p *= 2;
-    // }
-    // const homeTeam = match.home.team;
-    // const awayTeam = match.away.team;
-    //
-    // this.makeNewStep({
-    //   home: { id: homeTeam.id, rank: homeTeam.rank, points: homePoints + p },
-    //   away: { id: awayTeam.id, rank: awayTeam.rank, points: awayPoints - p },
-    // });
+    commit('INIT_STATE_LOADING')
+    try {
+      const worldRugbyData = await this.$axios.$get(
+        'https://cmsapi.pulselive.com/rugby/rankings/mru.json'
+      )
+
+      if (
+        !worldRugbyData.hasOwnProperty('entries') ||
+        !worldRugbyData.hasOwnProperty('effective') ||
+        !worldRugbyData.effective.hasOwnProperty('millis')
+      ) {
+        throw new Error('Data fetched from World Rugby is invalid')
+      }
+
+      dispatch('team/initTeams', worldRugbyData.entries)
+      commit('INIT_STATE_SUCCESS', worldRugbyData.effective.millis)
+    } catch (e) {
+      commit('INIT_STATE_FAILURE', e)
+    }
   }
+  // stepUp({ commit }) {
+  //   commit('STEP_UP')
+  // },
+  // stepDown({ commit }) {
+  //   commit('STEP_DOWN')
+  // },
+  // calculate({ dispatch }) {
+  // if (!this.$refs.matchForm.validate()) {
+  //   return;
+  // }
+  //
+  // const match = this.matches[this.index];
+  // const homeAdvantage = match.neutralGround ? 0 : 3;
+  // const homePoints = match.home.team.points[this.currentStep].raw;
+  // const awayPoints = match.away.team.points[this.currentStep].raw;
+  //
+  // let pointsDifference = homePoints + homeAdvantage - awayPoints;
+  // if (pointsDifference < -10) {
+  //   pointsDifference = -10;
+  // } else if (pointsDifference > 10) {
+  //   pointsDifference = 10;
+  // }
+  //
+  // const scoreDifference = match.home.score - match.away.score;
+  // let p;
+  // if (scoreDifference > 0) {
+  //   p = (-10 - scoreDifference) / 10;
+  // } else if (scoreDifference < 0) {
+  //   p = (10 - scoreDifference) / 10;
+  // } else {
+  //   p = (-scoreDifference) / 10;
+  // }
+  //
+  // if (Math.abs(scoreDifference) > 15) {
+  //   p *= 1.5;
+  // }
+  //
+  // if (match.worldCup) {
+  //   p *= 2;
+  // }
+  // const homeTeam = match.home.team;
+  // const awayTeam = match.away.team;
+  //
+  // this.makeNewStep({
+  //   home: { id: homeTeam.id, rank: homeTeam.rank, points: homePoints + p },
+  //   away: { id: awayTeam.id, rank: awayTeam.rank, points: awayPoints - p },
+  // });
+  // }
 }
--- store/team.js
+++ store/team.js
@@ -29,49 +29,49 @@ export const state = () => ({
 export const mutations = {
   INIT_TEAMS: (state, teams) => {
     state.teams = teams
-  },
-  RESET_STEPS: (state) => {
-    state.teams.forEach((team) => {
-      team.ranks = [team.ranks.shift()]
-      team.points = [team.points.shift()]
-    })
-  },
-  ADD_STEP_FOR_TEAM: (state, { id, rank, points }) => {
-    const team = state.teams.find((teamToFind) => teamToFind.id === id)
-    team.ranks.push(rank)
-    team.points.push({
-      raw: points,
-      rounded: roundingHelper.roundPoints(points)
-    })
-  },
-  INIT_NEW_STEP: (state, idsBlackList) => {
-    for (let i = 0; i < state.teams.length; i++) {
-      if (idsBlackList.includes(state.teams[i].id)) {
-        continue
-      }
-
-      const team = state.teams[i]
-      const points = team.points[team.points.length - 1]
-
-      team.ranks.push(team.ranks[team.ranks.length - 1])
-      team.points.push({
-        raw: points.raw,
-        rounded: points.rounded
-      })
-    }
   }
+  // RESET_STEPS: (state) => {
+  //   state.teams.forEach((team) => {
+  //     team.ranks = [team.ranks.shift()]
+  //     team.points = [team.points.shift()]
+  //   })
+  // },
+  // ADD_STEP_FOR_TEAM: (state, { id, rank, points }) => {
+  //   const team = state.teams.find((teamToFind) => teamToFind.id === id)
+  //   team.ranks.push(rank)
+  //   team.points.push({
+  //     raw: points,
+  //     rounded: roundingHelper.roundPoints(points)
+  //   })
+  // },
+  // INIT_NEW_STEP: (state, idsBlackList) => {
+  //   for (let i = 0; i < state.teams.length; i++) {
+  //     if (idsBlackList.includes(state.teams[i].id)) {
+  //       continue
+  //     }
+  //
+  //     const team = state.teams[i]
+  //     const points = team.points[team.points.length - 1]
+  //
+  //     team.ranks.push(team.ranks[team.ranks.length - 1])
+  //     team.points.push({
+  //       raw: points.raw,
+  //       rounded: points.rounded
+  //     })
+  //   }
+  // }
 }
 
 export const actions = {
   initTeams: ({ commit }, teams) => {
     commit('INIT_TEAMS', normalizeTeams(teams))
-  },
-  resetSteps: ({ commit }) => {
-    commit('RESET_STEPS')
-  },
-  makeNewStep: ({ commit }, payload) => {
-    commit('INIT_NEW_STEP', [payload.home.id, payload.away.id])
-    commit('ADD_STEP_FOR_TEAM', payload.home)
-    commit('ADD_STEP_FOR_TEAM', payload.away)
   }
+  // resetSteps: ({ commit }) => {
+  //   commit('RESET_STEPS')
+  // },
+  // makeNewStep: ({ commit }, payload) => {
+  //   commit('INIT_NEW_STEP', [payload.home.id, payload.away.id])
+  //   commit('ADD_STEP_FOR_TEAM', payload.home)
+  //   commit('ADD_STEP_FOR_TEAM', payload.away)
+  // }
 }
