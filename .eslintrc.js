1f1918433cfb44f665a8438b8c36d06317bc2777
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2020-03-22 19:53:20 +0100

    Disabling the no-prototype-builtins rule because the project is fairly simple

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,10 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    // disabling the no-prototype-builtins rule because this project is fairly simple :
+    // https://eslint.org/docs/rules/no-prototype-builtins
+    // You may want to turn this rule off if your code only touches objects with hardcoded keys, and you will never use
+    // an object that shadows an Object.prototype method or which does not inherit from Object.prototype.
+    'no-prototype-builtins': 'off'
   }
 }
