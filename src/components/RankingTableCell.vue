598eb6d4291fc0b39ba6c989d6379e5233372138
Arthur Falque Pierrotin <wallace@Wallace-MacBookAir-2020.local>
2020-06-06 22:55:46 +0200

    Delete everything

--- src/components/RankingTableCell.vue
+++
@@ -1,6 +0,0 @@
-<template functional v-bind="data.attrs">
-  <div class="d-flex flex-nowrap align-center">
-    <slot name="first"></slot>
-    <slot v-if="data.attrs.showSecondSlot" name="second"></slot>
-  </div>
-</template>
