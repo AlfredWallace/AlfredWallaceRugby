6bef596b6025d883db6af3a7cdd2694863c5f881
Alfred Wallace <7476515+AlfredWallace@users.noreply.github.com>
2021-03-27 15:44:53 +0100

    Adding a lot of tests + fixing a bug in the algo + renaming

--- src/services/calculateCoeff.js
+++ src/services/calculateCoeff.js
@@ -1,26 +1,26 @@
 export default function ({ match, homePoints, awayPoints }) {
   const homeAdvantage = match.neutralGround ? 0 : 3;
 
-  let pointsDifference = homePoints + homeAdvantage - awayPoints;
+  let weightedPointsDifference = homePoints + homeAdvantage - awayPoints;
 
-  if (pointsDifference < -10) {
-    pointsDifference = -10;
-  } else if (pointsDifference > 10) {
-    pointsDifference = 10;
+  if (weightedPointsDifference < -10) {
+    weightedPointsDifference = -10;
+  } else if (weightedPointsDifference > 10) {
+    weightedPointsDifference = 10;
   }
 
   const scoreDifference = match.home.score - match.away.score;
   let p;
 
   if (scoreDifference > 0) {
-    p = 1 - (pointsDifference / 10);
+    p = 1 - (weightedPointsDifference / 10);
   } else if (scoreDifference < 0) {
-    p = (1 + (pointsDifference / 10)) * (-1);
+    p = (1 + (weightedPointsDifference / 10)) * (-1);
   } else {
-    p = (pointsDifference / 10) * (-1);
+    p = (weightedPointsDifference / 10) * (-1);
   }
 
-  if (Math.abs(pointsDifference) > 15) {
+  if (Math.abs(scoreDifference) > 15) {
     p *= 1.5;
   }
 
--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -13,13 +13,95 @@ function makeTestMatch({
 }
 
 describe('Calculator', () => {
-  test('calculateCoeff', () => {
-    const match = makeTestMatch({ homeScore: 0, awayScore: 0, neutralGround: true });
-    const homePoints = 0;
-    const awayPoints = 0;
+  const dataSet = [
+    {
+      homeScore: 0, awayScore: 0, neutralGround: true, worldCup: false, homePoints: 0, awayPoints: 0, result: 0,
+    },
+    {
+      homeScore: 0, awayScore: 0, neutralGround: true, worldCup: true, homePoints: 0, awayPoints: 0, result: 0,
+    },
+    {
+      homeScore: 0, awayScore: 0, neutralGround: false, worldCup: false, homePoints: 0, awayPoints: 0, result: -0.3,
+    },
+    {
+      homeScore: 0, awayScore: 0, neutralGround: false, worldCup: true, homePoints: 0, awayPoints: 0, result: -0.6,
+    },
+    {
+      homeScore: 8, awayScore: 0, neutralGround: true, worldCup: false, homePoints: 0, awayPoints: 0, result: 1,
+    },
+    {
+      homeScore: 8, awayScore: 0, neutralGround: true, worldCup: true, homePoints: 0, awayPoints: 0, result: 2,
+    },
+    // {
+    //   homeScore: 8, awayScore: 0, neutralGround: false, worldCup: false, homePoints: 0, awayPoints: 0, result: 1,
+    // },
+    // {
+    //   homeScore: 8, awayScore: 0, neutralGround: false, worldCup: true, homePoints: 0, awayPoints: 0, result: 2,
+    // },
+    {
+      homeScore: 17, awayScore: 0, neutralGround: true, worldCup: false, homePoints: 0, awayPoints: 0, result: 1.5,
+    },
+    {
+      homeScore: 17, awayScore: 0, neutralGround: true, worldCup: true, homePoints: 0, awayPoints: 0, result: 3,
+    },
+    {
+      homeScore: 0, awayScore: 8, neutralGround: true, worldCup: false, homePoints: 0, awayPoints: 0, result: -1,
+    },
+    {
+      homeScore: 0, awayScore: 8, neutralGround: true, worldCup: true, homePoints: 0, awayPoints: 0, result: -2,
+    },
+    {
+      homeScore: 0, awayScore: 17, neutralGround: true, worldCup: false, homePoints: 0, awayPoints: 0, result: -1.5,
+    },
+    {
+      homeScore: 0, awayScore: 17, neutralGround: true, worldCup: true, homePoints: 0, awayPoints: 0, result: -3,
+    },
+    {
+      homeScore: 25, awayScore: 25, neutralGround: true, worldCup: false, homePoints: 13.37, awayPoints: 13.37, result: 0,
+    },
+    {
+      homeScore: 25, awayScore: 25, neutralGround: true, worldCup: true, homePoints: 13.37, awayPoints: 13.37, result: 0,
+    },
+    {
+      homeScore: 25, awayScore: 25, neutralGround: false, worldCup: false, homePoints: 13.37, awayPoints: 13.37, result: -0.3,
+    },
+    {
+      homeScore: 25, awayScore: 25, neutralGround: false, worldCup: true, homePoints: 13.37, awayPoints: 13.37, result: -0.6,
+    },
+    {
+      homeScore: 25, awayScore: 22, neutralGround: true, worldCup: false, homePoints: 13.37, awayPoints: 13.37, result: 1,
+    },
+    {
+      homeScore: 25, awayScore: 22, neutralGround: true, worldCup: true, homePoints: 13.37, awayPoints: 13.37, result: 2,
+    },
+    {
+      homeScore: 25, awayScore: 9, neutralGround: true, worldCup: false, homePoints: 13.37, awayPoints: 13.37, result: 1.5,
+    },
+    {
+      homeScore: 25, awayScore: 9, neutralGround: true, worldCup: true, homePoints: 13.37, awayPoints: 13.37, result: 3,
+    },
+    {
+      homeScore: 22, awayScore: 25, neutralGround: true, worldCup: false, homePoints: 13.37, awayPoints: 13.37, result: -1,
+    },
+    {
+      homeScore: 22, awayScore: 25, neutralGround: true, worldCup: true, homePoints: 13.37, awayPoints: 13.37, result: -2,
+    },
+    {
+      homeScore: 9, awayScore: 25, neutralGround: true, worldCup: false, homePoints: 13.37, awayPoints: 13.37, result: -1.5,
+    },
+    {
+      homeScore: 9, awayScore: 25, neutralGround: true, worldCup: true, homePoints: 13.37, awayPoints: 13.37, result: -3,
+    },
+  ];
 
-    const result = 0;
-
-    expect(calculateCoeff({ match, homePoints, awayPoints })).toEqual(result);
+  dataSet.forEach(({
+    homeScore, awayScore, neutralGround, worldCup, homePoints, awayPoints, result,
+  }) => {
+    test(`calculateCoeff: Team A (rank: ${homePoints}) scores ${homeScore} / Team B (rank: ${awayPoints}) scores ${awayScore} / neutral ground : ${neutralGround} / world cup : ${worldCup} / points exchanged should be : ${result}`, () => {
+      const match = makeTestMatch({
+        homeScore, awayScore, neutralGround, worldCup,
+      });
+      expect(Math.round(calculateCoeff({ match, homePoints, awayPoints }) * 1000) / 1000).toEqual(result);
+    });
   });
 });
